ec90d9dddb81a5cbd0722faed32aec88c106c349
Yusuf-Delvi <yusufmddelvi8@gmail.com>
2022-03-22 23:04:21 +0530

    authorize jwt from req header and pass it in context

---
+++ src/utils/auth.ts
@@ -0,0 +1,17 @@
+import * as jwt from 'jsonwebtoken';
+
+const APP_SECRET = process.env.APP_SECRET!;
+
+export interface AuthTokenPayload {
+	userId?: number;
+}
+
+export function decodeAuthHeader(authHeader: String): AuthTokenPayload {
+	const token = authHeader.replace('Bearer ', '');
+
+	if (!token) {
+		throw new Error('No token found');
+	}
+
+	return jwt.verify(token, APP_SECRET) as AuthTokenPayload;
+}
